d49367fceaca6458d90c496a1c6730d1321c428e
RaZ <jeuriqueribeiro@casadaeletricidade.com.br>
2016-08-05 02:04:06 -0300

    '2'

--- src/AbstractSyntaxTree/ASA.java
+++ src/AbstractSyntaxTree/ASA.java
@@ -4,5 +4,5 @@ package AbstractSyntaxTree;
  * Classe que representa a raiz da Árvore Sintática Abstrata
  */
 public abstract class ASA {
-
+	public abstract Object accept(XVisitor visitor);
 }
---
+++ src/AbstractSyntaxTree/XVisitor.java
@@ -0,0 +1,5 @@
+package AbstractSyntaxTree;
+
+public interface XVisitor {
+
+}
